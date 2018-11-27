b90a0b586f411c0b173c56ecdeba59d2f9a08be7
Diego Atance <32869877+KPsychO@users.noreply.github.com>
2018-11-27 01:54:19 +0100

    added the ultimate kitten killer code
    Pues nada, pillar sida

--- 2/suma.cpp
+++ 2/suma.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <stdlib.h>
+
+// 	PsychO - Concurso de código espaguetti - (Porfavor Pascal, no te autolesiones)
+
+#include <bits/stdc++.h>
 
 inline void in(long long int &n) {
 	n = 0;
@@ -23,24 +23,25 @@ int main(){
 	std::ofstream suma;
 	std::ofstream comp;
 	std::ofstream exec;
+	std::cout << "Introduzca 2 números naturales (0 incluido) para calcular la suma de forma que mueran gatitos. (-1 -1 para salir)\n";
 	INI:
 	in(a);
 	in(b);
-	(a+b)? ({goto EXEC;}) : ({goto FIN;});
+	((a == -1) && (b == -1))? ({goto FIN;}) : ({goto EXEC;});
 	EXEC:
 	sa = std::to_string(a);
 	sb = std::to_string(b);
-	fileName = sa + '0';
+	fileName = sa;
 	fileName += "+";
-	fileName += sb + '0';
+	fileName += sb;
 	fileName += ".cpp";
-	execFile = sa + '0';
+	execFile = sa;
 	execFile += "+";
-	execFile += sb + '0';
+	execFile += sb;
 	suma.open(fileName);
 	(suma.is_open())? ({goto File_open;}) : ({goto File_not_open;});
 	File_open:
-	suma << "#include <iostream>\ninline void out(int n) {int N = n, rev, count = 0; rev = N; if (N == 0) { putchar_unlocked('0'); return; } while ((rev % 10) == 0) { count++; rev /= 10; } rev = 0; while (N != 0) { rev = (rev << 3) + (rev << 1) + N % 10; N /= 10; } while (rev != 0) { putchar_unlocked(rev % 10 + '0'); rev /= 10; } while (count--) putchar_unlocked('0');} int main(){int a = " << a << ";  int b = " << b << "; int res = a + b; out(res); std::cout << std::endl; return 0;}";
+	suma << "#include <bits/stdc++.h> \n int TAM = 50;  inline void out(long long int n){ int N = n, rev, count = 0; rev = N; 	if (N == 0) { putchar_unlocked('0'); std::cout << std::endl;	return; } 	while ((rev % 10) == 0) { count++; rev /= 10; } 	rev = 0; 	while (N != 0) { rev = (rev << 3) + (rev << 1) + N % 10; N /= 10; } 	while (rev != 0) { putchar_unlocked(rev % 10 + '0'); rev /= 10; } 	while (count--) putchar_unlocked('0'); std::cout << std::endl;  }  int main(){  	srand(time(NULL));  	int a = " << a << "; int b = " << b << "; int va[TAM] = {rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, a}; 	int vb[TAM] = {rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, rand() % 10, b}; rand: std::random_shuffle(&va[0], &va[50]);	std::random_shuffle(&vb[0], &vb[50]);  (va[0] == a && vb[0] == b)? ({goto sum;}) : ({goto rand;}); sum:  out(va[0] + vb[0]); return 0;}";
 	suma.close();
 	compile = "g++ -Wall -O ";
 	compile += fileName;
